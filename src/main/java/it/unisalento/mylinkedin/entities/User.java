d7638d2d9db0b7ec9a89c4863c6b83e7917b9d2f
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-18 09:07:35 +0200

    Finite classi entities ma da ricontrollare (mancano dettagli relazioni)

--- src/main/java/it/unisalento/mylinkedin/entities/User.java
+++ src/main/java/it/unisalento/mylinkedin/entities/User.java
@@ -2,6 +2,7 @@ package it.unisalento.mylinkedin.entities;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 
 @Entity
@@ -19,8 +20,23 @@ public class User {
     public final static String REGISTRATION_ACCEPTED = "accepted";
     public final static String REGISTRATION_BLOCKED = "blocked";
 
-
     public User() {}
+
+    public User(int id, String name, String surname, String email, String password, Date birthDate, String description, List<ProfileImage> profileImage, List<Message> sentMessageList, List<Message> receivedMessageList, List<Comment> commentList, List<UserInterestedPost> userInterestedPostList, List<Post> postList) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.birthDate = birthDate;
+        this.description = description;
+        this.profileImage = profileImage;
+        this.sentMessageList = sentMessageList;
+        this.receivedMessageList = receivedMessageList;
+        this.commentList = commentList;
+        this.userInterestedPostList = userInterestedPostList;
+        this.postList = postList;
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -39,5 +55,122 @@ public class User {
     @Column(length = 500)
     String description;
 
-    //TODO: relazioni, costruttore, getter e setter e specializzazioni
+    @OneToMany
+    List<ProfileImage> profileImage;
+    @OneToMany
+    List<Message> sentMessageList;
+    @OneToMany
+    List<Message> receivedMessageList;
+    @OneToMany
+    List<Comment> commentList;
+    @OneToMany
+    List<UserInterestedPost> userInterestedPostList;
+    @OneToMany
+    List<Post> postList;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<ProfileImage> getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(List<ProfileImage> profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public List<Message> getSentMessageList() {
+        return sentMessageList;
+    }
+
+    public void setSentMessageList(List<Message> sentMessageList) {
+        this.sentMessageList = sentMessageList;
+    }
+
+    public List<Message> getReceivedMessageList() {
+        return receivedMessageList;
+    }
+
+    public void setReceivedMessageList(List<Message> receivedMessageList) {
+        this.receivedMessageList = receivedMessageList;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+
+    public List<UserInterestedPost> getUserInterestedPostList() {
+        return userInterestedPostList;
+    }
+
+    public void setUserInterestedPostList(List<UserInterestedPost> userInterestedPostList) {
+        this.userInterestedPostList = userInterestedPostList;
+    }
+
+    public List<Post> getPostList() {
+        return postList;
+    }
+
+    public void setPostList(List<Post> postList) {
+        this.postList = postList;
+    }
+
+    //TODO: relazioni
 }

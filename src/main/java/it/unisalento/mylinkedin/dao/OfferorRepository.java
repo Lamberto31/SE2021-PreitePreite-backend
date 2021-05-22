981b99de69e9feaa374cc7d633e89d8751891ccf
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-22 17:54:27 +0200

    Implementate query di tipo update

--- src/main/java/it/unisalento/mylinkedin/dao/OfferorRepository.java
+++ src/main/java/it/unisalento/mylinkedin/dao/OfferorRepository.java
@@ -3,9 +3,16 @@ package it.unisalento.mylinkedin.dao;
 import it.unisalento.mylinkedin.entities.Applicant;
 import it.unisalento.mylinkedin.entities.Offeror;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OfferorRepository extends JpaRepository<Offeror, Integer> {
     List<Offeror> findByStatus(String status);
+
+    @Modifying
+    @Query("update Offeror o set o.status = :status where o.id = :id")
+    void updateStatusRegistration(@Param("status") String status, @Param("id") int id);
 }
--- src/main/java/it/unisalento/mylinkedin/dao/PostRepository.java
+++ src/main/java/it/unisalento/mylinkedin/dao/PostRepository.java
@@ -2,11 +2,18 @@ package it.unisalento.mylinkedin.dao;
 
 import it.unisalento.mylinkedin.entities.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     List<Post> findByPrivate(boolean isPrivate);
-    //TODO: updateHiddenPost
+
+    @Modifying
+    @Query("update Post p set p.isHidden = :isHidden where p.id = :id")
+    void updateStatusRegistration(@Param("isHidden") boolean isHidden, @Param("id") int id);
+
     //TODO: filtraggio per skills
 }

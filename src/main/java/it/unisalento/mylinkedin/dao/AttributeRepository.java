50d6830d25640e05f9dc2e34752eaf59439dba7d
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-25 19:41:32 +0200

    Aggiunta annotation Repository nelle classi e ottimizzati import

--- src/main/java/it/unisalento/mylinkedin/dao/AttributeRepository.java
+++ src/main/java/it/unisalento/mylinkedin/dao/AttributeRepository.java
@@ -2,6 +2,8 @@ package it.unisalento.mylinkedin.dao;
 
 import it.unisalento.mylinkedin.entities.Attribute;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AttributeRepository extends JpaRepository<Attribute, Integer> {
 }
--- src/main/java/it/unisalento/mylinkedin/dao/MessageRepository.java
+++ src/main/java/it/unisalento/mylinkedin/dao/MessageRepository.java
@@ -2,6 +2,8 @@ package it.unisalento.mylinkedin.dao;
 
 import it.unisalento.mylinkedin.entities.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MessageRepository extends JpaRepository<Message, Integer> {
 }

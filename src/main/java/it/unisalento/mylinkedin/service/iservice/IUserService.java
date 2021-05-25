70930b54fd1cba7a8cfa566d99f069e2ea8a500c
Maddalena Preite <preitemadddi@gmail.com>
2021-05-25 18:55:29 +0200

    Definiti service di User, Offeror e Applicant

---
+++ src/main/java/it/unisalento/mylinkedin/service/iservice/IUserService.java
@@ -0,0 +1,31 @@
+package it.unisalento.mylinkedin.service.iservice;
+
+import it.unisalento.mylinkedin.entities.Applicant;
+import it.unisalento.mylinkedin.entities.Offeror;
+import it.unisalento.mylinkedin.entities.ProfileImage;
+import it.unisalento.mylinkedin.entities.User;
+
+import java.util.List;
+
+public interface IUserService {
+    List<User> getAll();
+
+    User save(User user);
+
+    User getById(int id);
+
+    void delete(int id);
+
+    User getByEmail(String email);
+
+
+    //APPLICANT
+    List<Applicant> getApplicantByStatus(String status);
+
+    //OFFEROR
+    List<Offeror> getOfferorByStatus(String status);
+
+    //OFFEROR E APPLICANT
+    void updateStatusRegistration(String status, int id);
+
+}

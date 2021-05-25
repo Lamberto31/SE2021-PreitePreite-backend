5da3994006f57eafe28a4e23938d1f0e0d762543
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-25 19:23:00 +0200

    Fix nome metodo findByUserCanPublish sbagliato

--- src/main/java/it/unisalento/mylinkedin/dao/StructureRepository.java
+++ src/main/java/it/unisalento/mylinkedin/dao/StructureRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface StructureRepository extends JpaRepository<Structure, Integer> {
-    List<Structure> getByUserCanPublish(String userCanPublish);
+    List<Structure> findByUserCanPublish(String userCanPublish);
 }

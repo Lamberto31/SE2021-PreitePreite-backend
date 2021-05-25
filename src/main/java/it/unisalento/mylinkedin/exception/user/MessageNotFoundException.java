f058d08634787836c907f3a30698e160b07c5b31
Maddalena Preite <preitemadddi@gmail.com>
2021-05-25 20:09:43 +0200

    Create classi not found exception per user service

---
+++ src/main/java/it/unisalento/mylinkedin/exception/user/MessageNotFoundException.java
@@ -0,0 +1,8 @@
+package it.unisalento.mylinkedin.exception.user;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Message not found")
+public class MessageNotFoundException extends Exception{
+}

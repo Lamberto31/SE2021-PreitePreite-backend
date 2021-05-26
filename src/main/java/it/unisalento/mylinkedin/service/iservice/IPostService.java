539c5242d273d30f582106f5cc6dcf7a050a7379
Maddalena Preite <preitemadddi@gmail.com>
2021-05-26 19:09:46 +0200

    Inserite eccezioni in interfaccia post service

--- src/main/java/it/unisalento/mylinkedin/service/iservice/IPostService.java
+++ src/main/java/it/unisalento/mylinkedin/service/iservice/IPostService.java
@@ -5,53 +5,55 @@ import it.unisalento.mylinkedin.entities.Attribute;
 import it.unisalento.mylinkedin.entities.Comment;
 import it.unisalento.mylinkedin.entities.Post;
 import it.unisalento.mylinkedin.entities.Structure;
+import it.unisalento.mylinkedin.exception.post.*;
+import it.unisalento.mylinkedin.exception.user.UserStatusInvalidException;
 
 import java.util.List;
 
 public interface IPostService {
     List<Post> getAll();
 
-    Post save(Post post);
+    Post save(Post post) throws SavingPostException;
 
-    Post getById(int id);
+    Post getById(int id) throws PostNotFoundException;
 
-    void delete(int id);
+    void delete(int id) throws PostNotFoundException;
 
-    List<Post> getByIsPrivate(boolean isPrivate);
+    List<Post> getByIsPrivate(boolean isPrivate) throws PostNotFoundException;
 
-    void updateIsHidden(boolean isHidden, int id);
+    void updateIsHidden(boolean isHidden, int id) throws PostNotFoundException, UserStatusInvalidException;
 
 
     //STRUCTURE
     List<Structure> getAllStructure();
 
-    Structure saveStructure(Structure structure);
+    Structure saveStructure(Structure structure) throws SavingStructureException;
 
-    Structure getStructureById(int id);
+    Structure getStructureById(int id) throws StructureNotFoundException;
 
-    void deleteStructure(int id);
+    void deleteStructure(int id) throws StructureNotFoundException;
 
-    List<Structure> getByUserCanPublish(String userCanPublish);
+    List<Structure> getByUserCanPublish(String userCanPublish) throws StructureNotFoundException;
 
 
     //ATTRIBUTE
     List<Attribute> getAllAttribute();
 
-    Attribute saveAttribute(Attribute attribute);
+    Attribute saveAttribute(Attribute attribute) throws SavingAttributeException;
 
-    Attribute getAttributeById(int id);
+    Attribute getAttributeById(int id) throws AttributeNotFoundException;
 
-    void deleteAttribute(int id);
+    void deleteAttribute(int id) throws AttributeNotFoundException;
 
 
     //COMMENT
     List<Comment> getAllComment();
 
-    Comment saveComment(Comment comment);
+    Comment saveComment(Comment comment) throws SavingCommentException;
 
-    Comment getCommentById(int id);
+    Comment getCommentById(int id) throws CommentNotFoundException;
 
-    void deleteComment(int id);
+    void deleteComment(int id) throws CommentNotFoundException;
 
 
 

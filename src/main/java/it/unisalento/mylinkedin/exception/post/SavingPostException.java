120f449844574a761cb0ee1bfd4b3e86a03f7d23
Maddalena Preite <preitemadddi@gmail.com>
2021-05-26 18:47:34 +0200

    Create classi saving exception per post service

---
+++ src/main/java/it/unisalento/mylinkedin/exception/post/SavingPostException.java
@@ -0,0 +1,8 @@
+package it.unisalento.mylinkedin.exception.post;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Saving error")
+public class SavingPostException extends Exception {
+}

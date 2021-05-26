36448665c86acbef89863dba176e56c3768c64b3
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-26 20:12:30 +0200

    Implementati tutti i servizi basilari di post service e inseriti autowired mancanti

--- src/main/java/it/unisalento/mylinkedin/service/serviceimpl/PostServiceImpl.java
+++ src/main/java/it/unisalento/mylinkedin/service/serviceimpl/PostServiceImpl.java
@@ -1,115 +1,164 @@
 package it.unisalento.mylinkedin.service.serviceimpl;
 
+import it.unisalento.mylinkedin.dao.AttributeRepository;
+import it.unisalento.mylinkedin.dao.CommentRepository;
 import it.unisalento.mylinkedin.dao.PostRepository;
+import it.unisalento.mylinkedin.dao.StructureRepository;
 import it.unisalento.mylinkedin.entities.Attribute;
 import it.unisalento.mylinkedin.entities.Comment;
 import it.unisalento.mylinkedin.entities.Post;
 import it.unisalento.mylinkedin.entities.Structure;
+import it.unisalento.mylinkedin.exception.InvalidValueException;
+import it.unisalento.mylinkedin.exception.post.*;
 import it.unisalento.mylinkedin.service.iservice.IPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-//TODO: Definire tutti i metodi e mettere Transactional Annotation
+//TODO: Mettere Transactional Annotation e definire metodi mancanti
 @Service
 public class PostServiceImpl implements IPostService {
 
     @Autowired
     PostRepository postRepository;
 
+    @Autowired
+    StructureRepository structureRepository;
+
+    @Autowired
+    AttributeRepository attributeRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
 
     @Override
     public List<Post> getAll() {
+        return postRepository.findAll();
+    }
+
+    @Override
+    public Post save(Post post) throws PostSavingException {
+        try {
+            return postRepository.save(post);
+        } catch (Exception e) {
+            throw new PostSavingException();
+        }
+    }
+
+    @Override
+    public Post getById(int id) throws PostNotFoundException {
+        return postRepository.findById(id).orElseThrow(PostNotFoundException::new);
+    }
+
+    @Override
+    public void delete(Post post) throws PostNotFoundException {
+        try {
+            postRepository.delete(post);
+        } catch (Exception e) {
+            throw new PostNotFoundException();
+        }
+    }
+
+    @Override
+    public List<Post> getByIsPrivate(boolean isPrivate) throws PostNotFoundException {
+        //TODO: implementare
         return null;
     }
 
     @Override
-    public Post save(Post post) {
-        return null;
-    }
-
-    @Override
-    public Post getById(int id) {
-        return null;
-    }
-
-    @Override
-    public void delete(Post post) {
-
-    }
-
-    @Override
-    public List<Post> getByIsPrivate(boolean isPrivate) {
-        return null;
-    }
-
-    @Override
-    public void updateIsHidden(boolean isHidden, int id) {
+    public void updateIsHidden(boolean isHidden, int id) throws PostNotFoundException, InvalidValueException {
+        //TODO: implementare
 
     }
 
     @Override
     public List<Structure> getAllStructure() {
-        return null;
+        return structureRepository.findAll();
     }
 
     @Override
-    public Structure saveStructure(Structure structure) {
-        return null;
+    public Structure saveStructure(Structure structure) throws StructureSavingException {
+        try {
+            return structureRepository.save(structure);
+        } catch (Exception e) {
+            throw new StructureSavingException();
+        }
     }
 
     @Override
-    public Structure getStructureById(int id) {
-        return null;
+    public Structure getStructureById(int id) throws StructureNotFoundException {
+        return structureRepository.findById(id).orElseThrow(StructureNotFoundException::new);
     }
 
     @Override
-    public void deleteStructure(Structure structure) {
-
+    public void deleteStructure(Structure structure) throws StructureNotFoundException {
+        try {
+            structureRepository.delete(structure);
+        } catch (Exception e) {
+            throw new StructureNotFoundException();
+        }
     }
 
     @Override
-    public List<Structure> getByUserCanPublish(String userCanPublish) {
+    public List<Structure> getByUserCanPublish(String userCanPublish) throws StructureNotFoundException {
+        //TODO: implementare
         return null;
     }
 
     @Override
     public List<Attribute> getAllAttribute() {
-        return null;
+        return attributeRepository.findAll();
     }
 
     @Override
-    public Attribute saveAttribute(Attribute attribute) {
-        return null;
+    public Attribute saveAttribute(Attribute attribute) throws AttributeSavingException {
+        try {
+            return attributeRepository.save(attribute);
+        } catch (Exception e) {
+            throw new AttributeSavingException();
+        }
     }
 
     @Override
-    public Attribute getAttributeById(int id) {
-        return null;
+    public Attribute getAttributeById(int id) throws AttributeNotFoundException {
+        return attributeRepository.findById(id).orElseThrow(AttributeNotFoundException::new);
     }
 
     @Override
-    public void deleteAttribute(Attribute attribute) {
-
+    public void deleteAttribute(Attribute attribute) throws AttributeNotFoundException {
+        try {
+            attributeRepository.delete(attribute);
+        } catch (Exception e) {
+            throw new AttributeNotFoundException();
+        }
     }
 
     @Override
     public List<Comment> getAllComment() {
-        return null;
+        return commentRepository.findAll();
     }
 
     @Override
-    public Comment saveComment(Comment comment) {
-        return null;
+    public Comment saveComment(Comment comment) throws CommentSavingException {
+        try {
+            return commentRepository.save(comment);
+        } catch (Exception e) {
+            throw new CommentSavingException();
+        }
     }
 
     @Override
-    public Comment getCommentById(int id) {
-        return null;
+    public Comment getCommentById(int id) throws CommentNotFoundException {
+        return commentRepository.findById(id).orElseThrow(CommentNotFoundException::new);
     }
 
     @Override
-    public void deleteComment(Comment comment) {
-
+    public void deleteComment(Comment comment) throws CommentNotFoundException {
+        try {
+            commentRepository.delete(comment);
+        } catch (Exception e) {
+            throw new CommentNotFoundException();
+        }
     }
 }

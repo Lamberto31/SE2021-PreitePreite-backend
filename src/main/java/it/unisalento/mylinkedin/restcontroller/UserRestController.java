19c83fe16b7196f499957ff62d2564577c38219e
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-30 16:04:28 +0200

    Finite API per Message

--- src/main/java/it/unisalento/mylinkedin/restcontroller/UserRestController.java
+++ src/main/java/it/unisalento/mylinkedin/restcontroller/UserRestController.java
@@ -1,13 +1,14 @@
 package it.unisalento.mylinkedin.restcontroller;
 
+import it.unisalento.mylinkedin.dto.MessageDTO;
+import it.unisalento.mylinkedin.dto.OfferorDTO;
 import it.unisalento.mylinkedin.dto.ProfileImageDTO;
 import it.unisalento.mylinkedin.dto.UserDTO;
+import it.unisalento.mylinkedin.entities.Message;
+import it.unisalento.mylinkedin.entities.Offeror;
 import it.unisalento.mylinkedin.entities.ProfileImage;
 import it.unisalento.mylinkedin.entities.User;
-import it.unisalento.mylinkedin.exception.user.ProfileImageNotFoundException;
-import it.unisalento.mylinkedin.exception.user.ProfileImageSavingException;
-import it.unisalento.mylinkedin.exception.user.UserNotFoundException;
-import it.unisalento.mylinkedin.exception.user.UserSavingException;
+import it.unisalento.mylinkedin.exception.user.*;
 import it.unisalento.mylinkedin.service.iservice.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -15,6 +16,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/user")
@@ -53,5 +56,34 @@ public class UserRestController {
         ProfileImage profileImageSaved = userService.saveProfileImage(profileImage);
         profileImageDTO.setId(profileImageSaved.getId());
         return profileImageDTO;
+    }
+
+    @GetMapping(value = "/Message/getById/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public MessageDTO getMessagetById(@PathVariable int id) throws MessageNotFoundException {
+
+        Message message = userService.getMessageById(id);
+        return new MessageDTO().convertToDto(message);
+    }
+
+    @PostMapping(value="/Message/save", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public MessageDTO saveMessage(@RequestBody @Valid MessageDTO messageDTO) throws ParseException, MessageSavingException {
+
+        Message message = new Message().convertToEntity(messageDTO);
+        Message messageSaved = userService.saveMessage(message);
+        messageDTO.setId(messageSaved.getId());
+        return messageDTO;
+    }
+
+    @GetMapping(value= "/Message/getBySenderAndReceiver/{senderId}/{receiverId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<MessageDTO> getMessageBySenderAndReceiver(@PathVariable("senderId") int senderId, @PathVariable("receiverId") int receiverId) throws MessageNotFoundException, UserNotFoundException {
+        User sender = userService.getById(senderId);
+        User receiver = userService.getById(receiverId);
+
+        List<Message> messageList = userService.getMessageBySenderAndReceiver(sender, receiver);
+        List<MessageDTO> messageDTOList = new ArrayList<>();
+        for(Message message: messageList) {
+            messageDTOList.add(new MessageDTO().convertToDto(message));
+        }
+        return messageDTOList;
     }
 }

6ef11fd1d820698b01d5901f73e1728940d8d115
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-31 19:37:21 +0200

    Corretto return dei repositories

--- src/main/java/it/unisalento/mylinkedin/dao/UserInterestedPostRepository.java
+++ src/main/java/it/unisalento/mylinkedin/dao/UserInterestedPostRepository.java
@@ -1,9 +1,6 @@
 package it.unisalento.mylinkedin.dao;
 
-import it.unisalento.mylinkedin.entities.Post;
-import it.unisalento.mylinkedin.entities.Structure;
-import it.unisalento.mylinkedin.entities.StructureAttribute;
-import it.unisalento.mylinkedin.entities.UserInterestedPost;
+import it.unisalento.mylinkedin.entities.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +8,5 @@ import java.util.List;
 
 @Repository
 public interface UserInterestedPostRepository extends JpaRepository<UserInterestedPost, Integer> {
-    List<UserInterestedPost> findUserByPost(Post post);
+    List<User> findUserByPost(Post post);
 }

d8a9a07a1bc8558cc29a9a3cfd0df28b8564ccc3
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-05-31 19:29:53 +0200

    Aggiunti repository StructureAttributeRepository UserInterestedPostRepository

---
+++ src/main/java/it/unisalento/mylinkedin/dao/StructureAttributeRepository.java
@@ -0,0 +1,13 @@
+package it.unisalento.mylinkedin.dao;
+
+import it.unisalento.mylinkedin.entities.Structure;
+import it.unisalento.mylinkedin.entities.StructureAttribute;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StructureAttributeRepository extends JpaRepository<StructureAttribute, Integer> {
+    List<StructureAttribute> findAttributeByStructure(Structure structure);
+}

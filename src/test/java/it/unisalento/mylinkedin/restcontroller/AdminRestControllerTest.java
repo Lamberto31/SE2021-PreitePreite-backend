b8112805dda34950e588fad26ba47ee8625d71eb
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-06-07 19:28:47 +0200

    Aggiunti test per i metodi Update di AdminRestController

--- src/test/java/it/unisalento/mylinkedin/restcontroller/AdminRestControllerTest.java
+++ src/test/java/it/unisalento/mylinkedin/restcontroller/AdminRestControllerTest.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import it.unisalento.mylinkedin.configurations.Constants;
 import it.unisalento.mylinkedin.dto.AttributeDTO;
 import it.unisalento.mylinkedin.dto.StructureDTO;
-import it.unisalento.mylinkedin.entities.Attribute;
-import it.unisalento.mylinkedin.entities.Structure;
-import it.unisalento.mylinkedin.entities.User;
+import it.unisalento.mylinkedin.entities.*;
 import it.unisalento.mylinkedin.exception.post.AttributeNotFoundException;
 import it.unisalento.mylinkedin.exception.post.AttributeSavingException;
 import it.unisalento.mylinkedin.exception.post.StructureNotFoundException;
@@ -52,6 +50,9 @@ public class AdminRestControllerTest {
     private Structure structure;
     private AttributeDTO attributeDTO;
     private Attribute attribute;
+    private Applicant applicant;
+    private Offeror offeror;
+    private Post post;
 
 
     @BeforeEach
@@ -91,6 +92,35 @@ public class AdminRestControllerTest {
 
         when(postServiceMock.getAttributeById(attribute.getId())).thenReturn(attribute);
 
+        this.applicant = new Applicant();
+        this.applicant.setId(1);
+        this.applicant.setName("testName");
+        this.applicant.setSurname("testSurname");
+        this.applicant.setEmail("emailtest@test.com");
+        this.applicant.setPassword("testPassword");
+        this.applicant.setBirthDate(Constants.SIMPLE_DATE_FORMAT.parse("01/01/2000 00:00"));
+        this.applicant.setDescription("testDescription");
+        this.applicant.setRegistrationDate(Constants.SIMPLE_DATE_FORMAT.parse("01/01/2000 00:00"));
+        this.applicant.setStatus(Constants.REGISTRATION_PENDING);
+        this.applicant.setFixedAttributes("testFixedAttributes");
+
+        this.offeror = new Offeror();
+        this.offeror.setId(1);
+        this.offeror.setName("testName");
+        this.offeror.setSurname("testSurname");
+        this.offeror.setEmail("emailtest@test.com");
+        this.offeror.setPassword("testPassword");
+        this.offeror.setBirthDate(Constants.SIMPLE_DATE_FORMAT.parse("01/01/2000 00:00"));
+        this.offeror.setDescription("testDescription");
+        this.offeror.setRegistrationDate(Constants.SIMPLE_DATE_FORMAT.parse("01/01/2000 00:00"));
+        this.offeror.setStatus(Constants.REGISTRATION_PENDING);
+
+        this.post = new Post();
+        this.post.setId(1);
+        this.post.setPubblicationDate(Constants.SIMPLE_DATE_FORMAT.parse("01/01/2000 00:00"));
+        this.post.setHidden(true);
+        this.post.setPrivate(true);
+        this.post.setData("testData");
     }
 
     @Test
@@ -102,20 +132,40 @@ public class AdminRestControllerTest {
 
     @Test
     void getApplicantByStatusTest() throws Exception {
-        mockMvc.perform(get(Constants.URI_ADMIN+Constants.URI_GETAPPLICANTBYSTATUS, Constants.REGISTRATION_PENDING)
+        mockMvc.perform(get(Constants.URI_ADMIN+Constants.URI_APPLICANT+Constants.URI_GETBYSTATUS, Constants.REGISTRATION_PENDING)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
     void getOfferorByStatusTest() throws Exception {
-        mockMvc.perform(get(Constants.URI_ADMIN+Constants.URI_GETOFFERORBYSTATUS, Constants.REGISTRATION_PENDING)
+        mockMvc.perform(get(Constants.URI_ADMIN+Constants.URI_OFFEROR+Constants.URI_GETBYSTATUS, Constants.REGISTRATION_PENDING)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     //TODO: Test updateStatusRegistration perchè dobbiamo fare put
+    @Test
+    void updateApplicantStatusRegistrationTest() throws Exception {
+        mockMvc.perform(put(Constants.URI_ADMIN +Constants.URI_APPLICANT + Constants.URI_UPDATESTATUSREGISTRATION, applicant.getId(), applicant.getStatus())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updateOfferorStatusRegistrationTest() throws Exception {
+        mockMvc.perform(put(Constants.URI_ADMIN +Constants.URI_OFFEROR + Constants.URI_UPDATESTATUSREGISTRATION, offeror.getId(), offeror.getStatus())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
     //TODO: Test updateIsHidden perchè dobbiamo fare put
+    @Test
+    void updatePostIsHiddenTest() throws Exception {
+        mockMvc.perform(put(Constants.URI_ADMIN +Constants.URI_POST + Constants.URI_UPDATEISHIDDEN, post.getId(), post.isHidden())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
 
     @Test
     void getAllStructureTest() throws Exception {

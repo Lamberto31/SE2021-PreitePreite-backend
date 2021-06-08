183a5d8bd854d5b326118ffffa3354002d5364f8
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-06-08 20:28:38 +0200

    Aggiunti metodi di test per GetById di IUserServiceTest Il metodo getByIdThrowsExTest non funziona probabilmente per l'orElseThrow del service

--- src/test/java/it/unisalento/mylinkedin/iservice/IUserServiceTest.java
+++ src/test/java/it/unisalento/mylinkedin/iservice/IUserServiceTest.java
@@ -3,6 +3,7 @@ package it.unisalento.mylinkedin.iservice;
 import it.unisalento.mylinkedin.configurations.Constants;
 import it.unisalento.mylinkedin.dao.*;
 import it.unisalento.mylinkedin.entities.User;
+import it.unisalento.mylinkedin.exception.user.UserNotFoundException;
 import it.unisalento.mylinkedin.exception.user.UserSavingException;
 import it.unisalento.mylinkedin.service.iservice.IUserService;
 import org.junit.jupiter.api.BeforeEach;
@@ -50,6 +51,7 @@ public class IUserServiceTest {
 
     private User user;
     private User wrongUser;
+    private int userId;
 
     @BeforeEach
     void init() throws ParseException {
@@ -65,6 +67,10 @@ public class IUserServiceTest {
         this.wrongUser = new User();
         when(userRepository.save(refEq(user))).thenReturn(user);
         when(userRepository.save(refEq(wrongUser))).thenThrow(IllegalArgumentException.class);
+
+        this.userId = 1;
+        when(userRepository.findById(userId)).thenReturn(java.util.Optional.ofNullable(user));
+        when(userRepository.findById(wrongUser.getId())).thenReturn(null);
     }
 
     @Test
@@ -81,6 +87,18 @@ public class IUserServiceTest {
     @Test
     void saveThrowsExTest() {
         Exception exp = assertThrows(UserSavingException.class, () -> userService.save(wrongUser));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void getByIdTest() throws UserNotFoundException {
+        User userFound = userService.getById(userId);
+        assertThat(user.equals(userFound));
+    }
+
+    @Test
+    void getByIdThrowsExTest() {
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.getById(wrongUser.getId()));
         assertThat(exp).isNotNull();
     }
 }

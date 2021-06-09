93c8b754676817ad423e2cf5b27dc32b190aa7bd
Maddalena Preite <preitemadddi@gmail.com>
2021-06-09 19:50:34 +0200

    Continuata classe di test IUserServiceTest

--- src/test/java/it/unisalento/mylinkedin/iservice/IUserServiceTest.java
+++ src/test/java/it/unisalento/mylinkedin/iservice/IUserServiceTest.java
@@ -2,6 +2,7 @@ package it.unisalento.mylinkedin.iservice;
 
 import it.unisalento.mylinkedin.configurations.Constants;
 import it.unisalento.mylinkedin.dao.*;
+import it.unisalento.mylinkedin.entities.Applicant;
 import it.unisalento.mylinkedin.entities.User;
 import it.unisalento.mylinkedin.exception.user.UserNotFoundException;
 import it.unisalento.mylinkedin.exception.user.UserSavingException;
@@ -15,6 +16,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -53,9 +56,12 @@ public class IUserServiceTest {
     private User user;
     private User wrongUser;
     private int userId;
+    private Applicant applicant;
+    private List<Applicant> applicantList;
+    private String applicantNotFoundStatus;
 
     @BeforeEach
-    void init() throws ParseException {
+    void init() throws ParseException, UserNotFoundException {
 
         this.user = new User();
         this.user.setName("testName");
@@ -76,6 +82,30 @@ public class IUserServiceTest {
 
         //when(userRepository.delete(refEq(wrongUser))).thenThrow(IllegalArgumentException.class);
         doThrow(new IllegalArgumentException()).when(userRepository).delete(wrongUser);
+
+        when(userRepository.findByEmail(user.getEmail())).thenReturn(user);
+
+        when(userRepository.findByEmail(wrongUser.getEmail())).thenThrow(IllegalArgumentException.class);
+
+        this.applicant = new Applicant();
+        this.applicant.setName("testName");
+        this.applicant.setSurname("testSurname");
+        this.applicant.setEmail("emailtest@test.com");
+        this.applicant.setPassword("testPassword");
+        this.applicant.setBirthDate(Constants.SIMPLE_DATE_FORMAT.parse("01/01/2000 00:00"));
+        this.applicant.setDescription("testDescription");
+        this.applicant.setStatus(Constants.REGISTRATION_PENDING);
+
+        when(applicantRepository.findById(userId)).thenReturn(java.util.Optional.ofNullable(applicant));
+
+        this.applicantList = new ArrayList<>();
+        applicantList.add(applicant);
+
+        when(applicantRepository.findByStatus(applicant.getStatus())).thenReturn(applicantList);
+
+        this.applicantNotFoundStatus = Constants.REGISTRATION_BLOCKED;
+
+        when(applicantRepository.findByStatus(applicantNotFoundStatus)).thenThrow(UserNotFoundException.class);
     }
 
     @Test
@@ -118,4 +148,42 @@ public class IUserServiceTest {
         Exception exp = assertThrows(UserNotFoundException.class, () -> userService.delete(wrongUser));
         assertThat(exp).isNotNull();
     }
+
+    @Test
+    void getByEmailTest() throws UserNotFoundException {
+        User userFound = userService.getByEmail(user.getEmail());
+        assertThat(user.equals(userFound));
+    }
+
+    @Test
+    void getByEmailThrowsExTest() {
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.getByEmail(wrongUser.getEmail()));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void getApplicantByIdTest() throws UserNotFoundException {
+        Applicant applicantFound = userService.getApplicantById(userId);
+        assertThat(applicant.equals(applicantFound));
+    }
+
+    @Test
+    void getApplicantByIdThrowsExTest() {
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.getApplicantById(wrongUser.getId()));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void getApplicantByStatusTest() throws UserNotFoundException {
+        List<Applicant> applicantFoundList = userService.getApplicantByStatus(applicant.getStatus());
+        assertThat(applicantList.equals(applicantFoundList));
+    }
+
+    @Test
+    void getApplicantByStatusThrowsExTest() {
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.getApplicantByStatus(applicantNotFoundStatus));
+        assertThat(exp).isNotNull();
+    }
+
+
 }

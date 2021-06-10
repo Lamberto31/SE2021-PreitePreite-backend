76b9f73393f5b7fd5a86246d11ced95a61675610
Maddalena Preite <preitemadddi@gmail.com>
2021-06-10 20:19:33 +0200

    Continuata classe IUserServiceTest Aggiunti test getById e getByStatus per offeror, updateStatusRegistration per offeror e applicant e crud per profileImage

--- src/test/java/it/unisalento/mylinkedin/iservice/IUserServiceTest.java
+++ src/test/java/it/unisalento/mylinkedin/iservice/IUserServiceTest.java
@@ -3,7 +3,12 @@ package it.unisalento.mylinkedin.iservice;
 import it.unisalento.mylinkedin.configurations.Constants;
 import it.unisalento.mylinkedin.dao.*;
 import it.unisalento.mylinkedin.entities.Applicant;
+import it.unisalento.mylinkedin.entities.Offeror;
+import it.unisalento.mylinkedin.entities.ProfileImage;
 import it.unisalento.mylinkedin.entities.User;
+import it.unisalento.mylinkedin.exception.InvalidValueException;
+import it.unisalento.mylinkedin.exception.user.ProfileImageNotFoundException;
+import it.unisalento.mylinkedin.exception.user.ProfileImageSavingException;
 import it.unisalento.mylinkedin.exception.user.UserNotFoundException;
 import it.unisalento.mylinkedin.exception.user.UserSavingException;
 import it.unisalento.mylinkedin.service.iservice.IUserService;
@@ -20,6 +25,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.refEq;
 import static org.mockito.Mockito.doThrow;
@@ -55,10 +61,15 @@ public class IUserServiceTest {
 
     private User user;
     private User wrongUser;
-    private int userId;
+    private int correctId;
     private Applicant applicant;
     private List<Applicant> applicantList;
-    private String applicantNotFoundStatus;
+    private String registeredUserNotFoundStatus;
+    private Offeror offeror;
+    private List<Offeror> offerorList;
+    private String wrongUserRegisteredStatus;
+    private ProfileImage profileImage;
+    private ProfileImage wrongProfileImage;
 
     @BeforeEach
     void init() throws ParseException, UserNotFoundException {
@@ -77,10 +88,9 @@ public class IUserServiceTest {
 
         when(userRepository.save(refEq(wrongUser))).thenThrow(IllegalArgumentException.class);
 
-        this.userId = 1;
-        when(userRepository.findById(userId)).thenReturn(java.util.Optional.ofNullable(user));
+        this.correctId = 1;
+        when(userRepository.findById(correctId)).thenReturn(java.util.Optional.ofNullable(user));
 
-        //when(userRepository.delete(refEq(wrongUser))).thenThrow(IllegalArgumentException.class);
         doThrow(new IllegalArgumentException()).when(userRepository).delete(wrongUser);
 
         when(userRepository.findByEmail(user.getEmail())).thenReturn(user);
@@ -96,16 +106,53 @@ public class IUserServiceTest {
         this.applicant.setDescription("testDescription");
         this.applicant.setStatus(Constants.REGISTRATION_PENDING);
 
-        when(applicantRepository.findById(userId)).thenReturn(java.util.Optional.ofNullable(applicant));
+        when(applicantRepository.findById(correctId)).thenReturn(java.util.Optional.ofNullable(applicant));
 
         this.applicantList = new ArrayList<>();
         applicantList.add(applicant);
 
         when(applicantRepository.findByStatus(applicant.getStatus())).thenReturn(applicantList);
 
-        this.applicantNotFoundStatus = Constants.REGISTRATION_BLOCKED;
+        this.registeredUserNotFoundStatus = Constants.REGISTRATION_BLOCKED;
 
-        when(applicantRepository.findByStatus(applicantNotFoundStatus)).thenThrow(UserNotFoundException.class);
+        when(applicantRepository.findByStatus(registeredUserNotFoundStatus)).thenThrow(UserNotFoundException.class);
+
+        this.offeror = new Offeror();
+        this.offeror.setName("testName");
+        this.offeror.setSurname("testSurname");
+        this.offeror.setEmail("emailtest@test.com");
+        this.offeror.setPassword("testPassword");
+        this.offeror.setBirthDate(Constants.SIMPLE_DATE_FORMAT.parse("01/01/2000 00:00"));
+        this.offeror.setDescription("testDescription");
+        this.offeror.setStatus(Constants.REGISTRATION_PENDING);
+
+        when(offerorRepository.findById(correctId)).thenReturn(java.util.Optional.ofNullable(offeror));
+
+        this.offerorList = new ArrayList<>();
+        offerorList.add(offeror);
+
+        when(offerorRepository.findByStatus(offeror.getStatus())).thenReturn(offerorList);
+
+        when(offerorRepository.findByStatus(registeredUserNotFoundStatus)).thenThrow(UserNotFoundException.class);
+
+        wrongUserRegisteredStatus = "wrong";
+
+        this.profileImage = new ProfileImage();
+        this.profileImage.setDescription("testDescription");
+        this.profileImage.setPubblicationDate(Constants.SIMPLE_DATE_FORMAT.parse("01/01/2000 00:00"));
+        this.profileImage.setImagePath("testImagePath");
+
+        when(profileImageRepository.save(refEq(profileImage))).thenReturn(profileImage);
+
+        this.wrongProfileImage = new ProfileImage();
+
+        when(profileImageRepository.save(refEq(wrongProfileImage))).thenThrow(IllegalArgumentException.class);
+
+        when(profileImageRepository.findById(correctId)).thenReturn(java.util.Optional.ofNullable(profileImage));
+
+        doThrow(new IllegalArgumentException()).when(profileImageRepository).delete(wrongProfileImage);
+
+
     }
 
     @Test
@@ -127,7 +174,7 @@ public class IUserServiceTest {
 
     @Test
     void getByIdTest() throws UserNotFoundException {
-        User userFound = userService.getById(userId);
+        User userFound = userService.getById(correctId);
         assertThat(user.equals(userFound));
     }
 
@@ -163,7 +210,7 @@ public class IUserServiceTest {
 
     @Test
     void getApplicantByIdTest() throws UserNotFoundException {
-        Applicant applicantFound = userService.getApplicantById(userId);
+        Applicant applicantFound = userService.getApplicantById(correctId);
         assertThat(applicant.equals(applicantFound));
     }
 
@@ -181,9 +228,109 @@ public class IUserServiceTest {
 
     @Test
     void getApplicantByStatusThrowsExTest() {
-        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.getApplicantByStatus(applicantNotFoundStatus));
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.getApplicantByStatus(registeredUserNotFoundStatus));
         assertThat(exp).isNotNull();
     }
+
+    @Test
+    void getOfferorByIdTest() throws UserNotFoundException {
+        Offeror offerorFound = userService.getOfferorById(correctId);
+        assertThat(offeror.equals(offerorFound));
+    }
+
+    @Test
+    void getOfferorByIdThrowsExTest() {
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.getOfferorById(wrongUser.getId()));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void getOfferorByStatusTest() throws UserNotFoundException {
+        List<Offeror> offerorFoundList = userService.getOfferorByStatus(offeror.getStatus());
+        assertThat(offerorList.equals(offerorFoundList));
+    }
+
+    @Test
+    void getOfferorByStatusThrowsExTest() {
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.getOfferorByStatus(registeredUserNotFoundStatus));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void updateApplicantStatusRegistrationTest() {
+        assertDoesNotThrow(() -> userService.updateApplicantStatusRegistration(applicant.getStatus(), correctId));
+    }
+
+    @Test
+    void updateApplicantStatusRegistrationThrowsUserNotFoundExTest() {
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.updateApplicantStatusRegistration(applicant.getStatus(), wrongUser.getId()));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void updateApplicantStatusRegistrationThrowsInvalidValueExTest() {
+        Exception exp = assertThrows(InvalidValueException.class, () -> userService.updateApplicantStatusRegistration(wrongUserRegisteredStatus, correctId));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void updateOfferorStatusRegistrationTest() {
+        assertDoesNotThrow(() -> userService.updateOfferorStatusRegistration(offeror.getStatus(), correctId));
+    }
+
+    @Test
+    void updateOfferorStatusRegistrationThrowsUserNotFoundExTest() {
+        Exception exp = assertThrows(UserNotFoundException.class, () -> userService.updateOfferorStatusRegistration(offeror.getStatus(), wrongUser.getId()));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void updateOfferorStatusRegistrationThrowsInvalidValueExTest() {
+        Exception exp = assertThrows(InvalidValueException.class, () -> userService.updateOfferorStatusRegistration(wrongUserRegisteredStatus, correctId));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void getAllProfileImageTest() {
+        assertThat(userService.getAllProfileImage()).isNotNull();
+    }
+
+    @Test
+    void saveProfileImageTest() throws ProfileImageSavingException {
+        ProfileImage profileImageSaved = userService.saveProfileImage(profileImage);
+        assertThat(profileImage.equals(profileImageSaved));
+    }
+
+    @Test
+    void saveProfileImageThrowsExTest() {
+        Exception exp = assertThrows(ProfileImageSavingException.class, () -> userService.saveProfileImage(wrongProfileImage));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void getProfileImageByIdTest() throws ProfileImageNotFoundException {
+        ProfileImage profileImageFound = userService.getProfileImageById(correctId);
+        assertThat(profileImage.equals(profileImageFound));
+    }
+
+    @Test
+    void getProfileImageByIdThrowsExTest() {
+        Exception exp = assertThrows(ProfileImageNotFoundException.class, () -> userService.getProfileImageById(wrongProfileImage.getId()));
+        assertThat(exp).isNotNull();
+    }
+
+    @Test
+    void deleteProfileImageTest() throws ProfileImageNotFoundException {
+        ProfileImage profileImageDeleted = userService.deleteProfileImage(profileImage);
+        assertThat(profileImage.equals(profileImageDeleted));
+    }
+
+    @Test
+    void deleteProfileImageThrowsExTest() {
+        Exception exp = assertThrows(ProfileImageNotFoundException.class, () -> userService.deleteProfileImage(wrongProfileImage));
+        assertThat(exp).isNotNull();
+    }
+
 
 
 }

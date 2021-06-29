833660d516e71a45514ed948a6662f5434b9fd07
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-06-29 22:43:04 +0200

    Aggiunta configurazione security

---
+++ src/main/java/it/unisalento/mylinkedin/configurations/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package it.unisalento.mylinkedin.configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers(Constants.URI_ADMIN + "/**").hasRole("ADMIN")
+                .antMatchers(Constants.URI_REGISTEREDUSER + "/**").hasAnyRole("ADMIN", "REGISTEREDUSER")
+                .antMatchers("/").permitAll()
+                .and().formLogin()
+                .successHandler((request, response, authentication) -> {
+                    //Do nothing
+                });
+        http.cors().and().csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
+

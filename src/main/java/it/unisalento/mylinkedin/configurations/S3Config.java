404d69fae7a652a3a87a50af01a8d1ac32a3b001
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-07-03 15:50:37 +0200

    Cambiate credenziali per includere session token

--- src/main/java/it/unisalento/mylinkedin/configurations/S3Config.java
+++ src/main/java/it/unisalento/mylinkedin/configurations/S3Config.java
@@ -2,6 +2,7 @@ package it.unisalento.mylinkedin.configurations;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -18,13 +19,16 @@ public class S3Config {
     @Value("${jsa.aws.secret_access_key}")
     private String awsKey;
 
+    @Value("${jsa.aws.session_token}")
+    private String sessionToken;
+
     @Value("${jsa.s3.region}")
     private String region;
 
     @Bean
     public AmazonS3 s3client() {
 
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(awsId, awsKey);
+        BasicSessionCredentials awsCreds = new BasicSessionCredentials(awsId, awsKey, sessionToken);
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
                 .withRegion(Regions.fromName(region))
                 .withCredentials(new AWSStaticCredentialsProvider(awsCreds))

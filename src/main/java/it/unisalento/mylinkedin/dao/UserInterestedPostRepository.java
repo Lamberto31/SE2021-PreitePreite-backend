276739be432b2304b9f935676b51193b11c609b9
Lamberto Preite <78571193+Lamberto31@users.noreply.github.com>
2021-07-25 12:25:59 +0200

    Aggiunta query per aggiornare userInterestedPost isNotified

--- src/main/java/it/unisalento/mylinkedin/dao/UserInterestedPostRepository.java
+++ src/main/java/it/unisalento/mylinkedin/dao/UserInterestedPostRepository.java
@@ -3,7 +3,9 @@ package it.unisalento.mylinkedin.dao;
 import it.unisalento.mylinkedin.entities.User;
 import it.unisalento.mylinkedin.entities.UserInterestedPost;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,5 +15,9 @@ public interface UserInterestedPostRepository extends JpaRepository<UserInterest
     @Query("select u.user from UserInterestedPost u where u.post.id= :postId")
     List<User> findUserByPost(int postId);
 
-    List<UserInterestedPost> findByIsAlreadyNotified(boolean alreadyNotified);
+    List<UserInterestedPost> findByIsNotified(boolean isNotified);
+
+    @Modifying
+    @Query("update UserInterestedPost u set u.isNotified = :isNotified where u.id = :id")
+    void updateIsHidden(@Param("isNotified") boolean isNotified, @Param("id") int id);
 }
